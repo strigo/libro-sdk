f5578e94068febaf59d41d17e6a30af8d3479dc4
Dvir Malka <dvir.malka@strigo.io>
2022-02-13 17:48:31 +0200

    Fix build system

--- esbuild-dev.js
+++ esbuild-dev.js
@@ -1,30 +1,29 @@
-import 'dotenv/config'
-import esbuild from 'esbuild';
-import envFilePlugin from 'esbuild-envfile-plugin';
-import serve, { error, log } from 'create-serve';
+import "dotenv/config";
+import esbuild from "esbuild";
+import serve, { error, log } from "create-serve";
 
-const OUT_DIR = 'dist/development';
+const OUT_DIR = "dist/development";
 
 // Generate CSS/JS Builds
 esbuild
   .build({
-    logLevel: 'debug',
+    logLevel: "debug",
     metafile: true,
-    entryPoints: ['src/styles/strigo.css', 'src/strigo.sdk.ts'],
-    platform: 'browser',
-    plugins: [envFilePlugin],
+    entryPoints: ["src/styles/strigo.css", "src/strigo.sdk.ts"],
+    platform: "browser",
+    // plugins: [envFilePlugin],
     outdir: OUT_DIR,
     bundle: true,
+    define: { SDK_HOSTING_PORT: `"${process.env.SDK_HOSTING_PORT}"` },
     watch: {
       onRebuild(err) {
-        
         serve.update();
-        err ? error('× Failed') : log('✓ Updated');
+        err ? error("× Failed") : log("✓ Updated");
       }
     }
   })
   .then(() => {
-    console.log('⚡ Styles & Scripts Compiled! ⚡ ');
+    console.log("⚡ Styles & Scripts Compiled! ⚡ ");
   })
   .catch(() => process.exit(1));
 
