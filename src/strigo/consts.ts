8286a7b1dff18532e0c2a9ada480f7700de2fd90
Dvir Malka <dvir.malka@strigo.io>
2022-02-17 09:01:58 +0200

    Fix sdk version for compiled CSS

--- src/strigo/consts.ts
+++ src/strigo/consts.ts
@@ -1,7 +1,6 @@
 export const BASE_STRIGO_URL = "strigo.io";
 export const LOCAL_STRIGO_URL = "localhost:3000";
 
-//TODO: get CSS from build
-export const CSS_URL = "https://cdn.jsdelivr.net/gh/strigo/strigo-sdk/dist/production/styles/strigo.min.css";
+export const CSS_URL = "https://cdn.jsdelivr.net/gh/strigo/strigo-sdk@latest/dist/production/styles/strigo.min.css";
 export const STRIGO_IFRAME_CLASSES = ["strigo-exercises"];
 export const ORIGINAL_WEBSITE_IFRAME_CLASSES = ["orig-iframe"];
