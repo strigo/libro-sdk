0b7d4bdd7ff597ff9a1125a46cb08d17602136e2
Dvir Malka <dvir.malka@strigo.io>
2022-02-17 09:39:41 +0200

    Add jsdelivr purge CDN

---
+++ purge-jsdelivr.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const FILES_TO_PURGE = [
+  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@latest/dist/production/strigo.sdk.min.js",
+  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@latest/dist/production/styles/strigo.min.css"
+];
+
+async function purge() {
+  for (const fileUrl of FILES_TO_PURGE) {
+    console.log(fileUrl);
+    const response = await axios.get(fileUrl, {});
+    console.log(JSON.stringify(response?.data,null,2));
+  }
+}
+
+purge();
