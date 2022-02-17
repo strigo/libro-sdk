b704b47787b6a822a4756d64963fbfdc76dbbf8a
Dvir Malka <dvir.malka@strigo.io>
2022-02-17 11:40:03 +0200

    Add minimum pane size on desktop

--- src/modules/document/document.ts
+++ src/modules/document/document.ts
@@ -42,3 +42,7 @@ export function appendIFrame(params: AppendIframeParams) {
 export function reloadPage(): void {
   location.reload();
 }
+
+export function isMobile(): boolean {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
--- src/strigo/consts.ts
+++ src/strigo/consts.ts
@@ -4,3 +4,6 @@ export const LOCAL_STRIGO_URL = "localhost:3000";
 export const CSS_URL = "https://cdn.jsdelivr.net/gh/strigo/strigo-sdk@latest/dist/production/styles/strigo.min.css";
 export const STRIGO_IFRAME_CLASSES = ["strigo-exercises"];
 export const ORIGINAL_WEBSITE_IFRAME_CLASSES = ["orig-iframe"];
+
+export const MINIMUM_PANE_SIZE_MOBILE = 0;
+export const MINIMUM_PANE_SIZE_DESKTOP = 450;
