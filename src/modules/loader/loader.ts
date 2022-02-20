e326c9ce856c0e22012c94458ba574d6f95b3ab1
Dvir Malka <dvir.malka@strigo.io>
2022-02-20 16:20:16 +0200

    Change loader

--- src/modules/loader/loader.ts
+++ src/modules/loader/loader.ts
@@ -1,14 +1,18 @@
 const SPINNER = `
-<?xml version="1.0" encoding="utf-8"?>
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto; animation-play-state: running; animation-delay: 0s;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-<circle cx="50" cy="50" r="0" fill="none" stroke="#ffffff" stroke-width="2" style="animation-play-state: running; animation-delay: 0s;">
-  <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s" style="animation-play-state: running; animation-delay: 0s;"></animate>
-  <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s" style="animation-play-state: running; animation-delay: 0s;"></animate>
-</circle><circle cx="50" cy="50" r="0" fill="none" stroke="#f1f1f9" stroke-width="2" style="animation-play-state: running; animation-delay: 0s;">
-  <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s" style="animation-play-state: running; animation-delay: 0s;"></animate>
-  <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s" style="animation-play-state: running; animation-delay: 0s;"></animate>
-</circle>
-</svg>`;
+<div class="circle-loader">
+  <svg class="circular" viewBox="25 25 50 50">
+  <circle
+    class="path"
+    cx="50"
+    cy="50"
+    r="20"
+    fill="none"
+    stroke-width="2"
+    stroke-miterlimit="10"
+  />
+  </svg>
+</div>
+`;
 
 export function addLoader() {
   // Add Spinner
