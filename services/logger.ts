4c9eea0e0288dd0162101201f91802b54de84188
Omry Zobel <omry@strigo.io>
2022-03-27 17:10:24 +0300

    Fetch log config from strigo app

--- services/logger.ts
+++ services/logger.ts
@@ -14,61 +14,65 @@ class Logger {
     this.url = config.url;
     this.token = config.token;
   }
+
+  logToRemote(severity: string, message: string, context: {}) {
+    fetch(this.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.token}`
+      },
+      body: JSON.stringify({
+        severity,
+        message,
+        context,
+        dt: new Date().toISOString()
+      })
+    }).then((result) => {
+      if (!result.ok) {
+        console.warn("Logging to Strigo failed");
+      }
+    });
+  }
+
+  logToConsole(severity: string, message: string, context: {}) {
+    const enrichedMessage = `${new Date().toISOString()} - ${message}`;
+    console[severity](enrichedMessage, context ? `\n${JSON.stringify(context)}` : "");
+  }
+
   /**
-   * Writes a log message to the browser's console.
-   * The severity level is omitted here as browsers colorize according to severity anyway.
-   *
-   * Example (local console):
-   * 2020-04-15T06:52:21.905Z - Message...
-   *   key1=value1
-   *   key2=value2
+   * Writes a log message to a remote logging solution and the browser's console.
    *
    * @param {String} severity a `console.log` acceptable severity level (negating `trace`).
    * @param {String} message the message to convey.
    * @param {Object} context the object to provide as context.
    */
-  log(severity: string, message: any, context: {}) {
+  log(severity: string, message: string, context: {}) {
     try {
       if (this.url && this.token && !configManager.getConfig().development) {
-        fetch(this.url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${this.token}`
-          },
-          body: JSON.stringify({
-            severity,
-            message,
-            context,
-            dt: new Date().toISOString()
-          })
-        });
-
-        //TODO: do we wanna log to console too? maybe only from a certain severity level? or only for dev?
-        return;
+        this.logToRemote(severity, message, context);
       }
 
-      // If no url or token is provided, fallback to console.log
-      const enrichedMessage = `${new Date().toISOString()} - ${message}`;
-      console[severity](enrichedMessage, context ? `\n${JSON.stringify(context)}` : "");
+      // also console.log always
+      this.logToConsole(severity, message, context);
     } catch (err) {
       console.log("Logging error:", err);
     }
   }
 
-  debug(message: any, context = {}) {
+  debug(message: string, context = {}) {
     this.log("debug", message, context);
   }
 
-  info(message: any, context = {}) {
+  info(message: string, context = {}) {
     this.log("info", message, context);
   }
 
-  warn(message: any, context = {}) {
+  warn(message: string, context = {}) {
     this.log("warn", message, context);
   }
 
-  error(message: any, context = {}) {
+  error(message: string, context = {}) {
     this.log("error", message, context);
   }
 }
