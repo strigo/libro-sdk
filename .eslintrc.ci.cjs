bff7c5513315c40a69622ac01746bec2af8807c4
Vladimir Sorokin <vova@strigo.io>
2022-04-18 06:33:55 +0300

    Add linting to CI

---
+++ .eslintrc.ci.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  'extends': './.eslintrc.cjs',
+  'rules': {
+    'prettier/prettier': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
