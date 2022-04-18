ea5ff89ed98a36cc45120d068f1589240bc2455d
Vladimir Sorokin <vova@strigo.io>
2022-04-18 06:33:55 +0300

    Add linting to CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   rules: {
     'prettier/prettier': 'warn',
@@ -120,5 +120,5 @@ module.exports = {
     'curly': ['error', 'all'],
     'no-plusplus': 'off',
   },
-  ignorePatterns: ['node_modules/', '*.js'],
+  ignorePatterns: ['node_modules/', '*.js', '*.cjs'],
 };
