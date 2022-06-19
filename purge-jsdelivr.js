798c99bed4c7dd9cbbbbf68d2f3aceb3d5e91ec5
Dvir Malka <dvir.malka@strigo.io>
2022-06-19 15:37:53 +0300

    [semver:patch] Minor purge fix

--- purge-jsdelivr.js
+++ purge-jsdelivr.js
@@ -1,12 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 const FILES_TO_PURGE = [
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/strigo.sdk.min.js",
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/styles/strigo.min.css",
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/styles/strigo-widget.min.css",
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/strigo.sdk.js",
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/styles/strigo.css",
-  "https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/styles/strigo-widget.css"
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/strigo.sdk.min.js',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/styles/strigo.min.css',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/styles/strigo-widget.min.css',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/production/styles/strigo-assessment-recorder.min.css',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/strigo.sdk.js',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/styles/strigo.css',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/styles/strigo-widget.css',
+  'https://purge.jsdelivr.net/gh/strigo/strigo-sdk@master/dist/development/styles/strigo-assessment-recorder.css',
 ];
 
 async function purge() {
