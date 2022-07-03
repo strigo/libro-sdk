e626796216e195b04dc21fc2bbfe9786fafd422f
Dvir Malka <dvir.malka@strigo.io>
2022-07-03 15:58:51 +0300

    [semver:patch] Build new version

--- dist/development/strigo.sdk.js
+++ dist/development/strigo.sdk.js
@@ -11640,7 +11640,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateCssURL(development, version) {
     if (development) {
-      return `http://localhost:${"7000"}/styles/strigo.css`;
+      return `http://localhost:${SDK_HOSTING_PORT}/styles/strigo.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo.min.css`;
@@ -11649,7 +11649,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateWidgetCssURL(development, version) {
     if (development) {
-      return `http://localhost:${"7000"}/styles/strigo-widget.css`;
+      return `http://localhost:${SDK_HOSTING_PORT}/styles/strigo-widget.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-widget.min.css`;
@@ -11658,7 +11658,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateAcademyHatCssURL(development, version) {
     if (development) {
-      return `http://localhost:${"7000"}/styles/strigo-academy-hat.css`;
+      return `http://localhost:${SDK_HOSTING_PORT}/styles/strigo-academy-hat.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-academy-hat.min.css`;
@@ -11667,7 +11667,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateRecorderCssURL(development, version) {
     if (development) {
-      return `http://localhost:${"7000"}/styles/strigo-assessment-recorder.css`;
+      return `http://localhost:${SDK_HOSTING_PORT}/styles/strigo-assessment-recorder.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-assessment-recorder.min.css`;
@@ -11675,7 +11675,7 @@ ${JSON.stringify(parsedContext)}` : "");
     return `${CDN_BASE_PATH}@master/dist/production/styles/strigo-assessment-recorder.min.css`;
   }
   function generateAssessmentRecorderURL(development) {
-    return development ? `http://localhost:${"7015"}` : ASSESSMENT_RECORDER_URL;
+    return development ? `http://localhost:${RECORDER_HOSTING_PORT}` : ASSESSMENT_RECORDER_URL;
   }
   function isRecordingUrlParamExists() {
     const { search } = window.location;
@@ -12412,103 +12412,6 @@ ${JSON.stringify(parsedContext)}` : "");
     }
   }
 
-  // src/modules/no-code-assessment/no-code-assessment.ts
-  var observerOptions = {
-    subtree: true,
-    characterData: true,
-    childList: true
-  };
-  var addDocumentObserver = function(windowElement) {
-    const documentElement = windowElement.document;
-    const assessments = getAssessmentsStorageData().assessments;
-    const locationHandlers = {};
-    const assessmentStatuses = {};
-    const observerHandler = function() {
-      assessments.forEach((assessment) => {
-        const { recordedAssessment, challengeSuccessEvent, _id } = assessment;
-        const { actionType, expectedText } = recordedAssessment;
-        const locationElementProfile = recordedAssessment?.locationElement?.profile;
-        if (assessmentStatuses?.[_id] === "success" || !locationElementProfile) {
-          return;
-        }
-        assessmentStatuses[_id] = "pending";
-        let locationElement;
-        try {
-          const locationElementSelector = getElementSelector(locationElementProfile);
-          locationElement = documentElement.querySelector(locationElementSelector);
-        } catch (err) {
-          return;
-        }
-        switch (actionType) {
-          case "added-item" /* ADDED_ITEM */: {
-            if (locationHandlers[_id]?.observer && locationElement === locationHandlers[_id].element) {
-              locationHandlers[_id].element = locationElement;
-              LoggerInstance.info("Same reference - no need to observe again");
-              return;
-            }
-            if (locationElement[_id]?.observer) {
-              locationElement[_id].observer.observe(locationElement, observerOptions);
-              LoggerInstance.info("DOM Reference have changed - observing again");
-              return;
-            }
-            locationHandlers[_id] = {
-              element: locationElement,
-              observer: new MutationObserver(function() {
-                const exampleElementProfile = this.assessment.recordedAssessment?.exampleElement?.profile;
-                if (!exampleElementProfile) {
-                  return;
-                }
-                let exampleElementSelector;
-                try {
-                  exampleElementSelector = getElementSelector(exampleElementProfile, true);
-                } catch {
-                  return;
-                }
-                const currentExampleElementCount = locationElement.querySelectorAll(exampleElementSelector)?.length || 0;
-                const previousExampleElementCount = window.sessionStorage.getItem(_id);
-                if (!previousExampleElementCount) {
-                  window.sessionStorage.setItem(_id, currentExampleElementCount);
-                  return;
-                }
-                if (currentExampleElementCount > parseInt(previousExampleElementCount)) {
-                  assessmentStatuses[_id] = "success";
-                  windowElement.Strigo.sendEvent(challengeSuccessEvent);
-                  locationHandlers[_id].observer.disconnect();
-                  delete locationHandlers[_id];
-                }
-              }.bind({ assessment }))
-            };
-            locationHandlers[_id].observer.observe(locationElement, observerOptions);
-            break;
-          }
-          case "text-change" /* TEXT_CHANGE */: {
-            if (locationElement?.innerText?.includes(expectedText) || locationElement?.value?.includes(expectedText)) {
-              assessmentStatuses[_id] = "success";
-              LoggerInstance.info(`sent event ${challengeSuccessEvent}`);
-              windowElement.Strigo.sendEvent(challengeSuccessEvent);
-            }
-            break;
-          }
-          default: {
-            break;
-          }
-        }
-      });
-    };
-    if (!windowElement?.strigoObserver?.observer) {
-      windowElement.strigoObserver = {
-        observer: new MutationObserver(observerHandler),
-        element: windowElement.document.body
-      };
-      windowElement?.strigoObserver?.observer?.observe(windowElement.document.body, observerOptions);
-      return;
-    }
-    if (windowElement.strigoObserver.element !== windowElement.document.body) {
-      windowElement.strigoObserver.element = windowElement.document.body;
-      windowElement.strigoObserver.observer.observe(windowElement.document.body, observerOptions);
-    }
-  };
-
   // src/modules/widgets/overlay.ts
   var import_interactjs = __toESM(require_interact_min(), 1);
   var MINIMUM_WIDTH = 342;
@@ -12560,7 +12463,6 @@ ${JSON.stringify(parsedContext)}` : "");
       const academyPlayerFrame = createWidget(generateStrigoIframeURL(getConfig()));
       this.initEventListeners(academyPlayerFrame);
       console.log("adding observer");
-      this.documentObserver = addDocumentObserver(window);
       console.log("observer added");
       setupResizeFunctionality();
     }
@@ -12658,7 +12560,6 @@ ${JSON.stringify(parsedContext)}` : "");
   function initChildEventListeners(childIframe) {
     const originalHost = getConfigValue("initSite")?.host;
     childIframe.addEventListener("load", function() {
-      addDocumentObserver(childIframe.contentWindow);
       try {
         const currentHost = this.contentWindow.location.host;
         if (currentHost !== originalHost) {
