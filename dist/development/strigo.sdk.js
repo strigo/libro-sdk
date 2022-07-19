e4f46adc22e8e70f2345936b05df4461179e1df3
Dvir Malka <dvir.malka@strigo.io>
2022-07-19 12:10:13 +0300

    [semver:patch] Fix open widget

--- dist/development/strigo.sdk.js
+++ dist/development/strigo.sdk.js
@@ -11098,12 +11098,12 @@ ${JSON.stringify(parsedContext)}` : "");
     hostingAppWindow.document.getElementById("strigo-widget").remove();
   }
   function openWidget(hostingAppWindow) {
-    const widget = hostingAppWindow.document.getElementById("strigo-widget");
+    const widget = document.getElementById("strigo-widget");
     widget.classList.add("slide-in");
     widget.classList.add("loaded");
-    const collapseDiv = hostingAppWindow.document.getElementById("strigo-collapse-div");
+    const collapseDiv = document.getElementById("strigo-collapse-div");
     collapseDiv.classList.remove("slide-in");
-    const academyHat = hostingAppWindow.document.getElementById("strigo-academy-hat");
+    const academyHat = document.getElementById("strigo-academy-hat");
     academyHat.classList.remove("slide-in");
   }
 
@@ -11645,7 +11645,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7005"}/styles/strigo.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo.min.css`;
@@ -11654,7 +11654,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateWidgetCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7005"}/styles/strigo-widget.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-widget.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-widget.min.css`;
@@ -11663,7 +11663,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateAcademyHatCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7005"}/styles/strigo-academy-hat.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-academy-hat.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-academy-hat.min.css`;
@@ -11672,7 +11672,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateRecorderCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7005"}/styles/strigo-assessment-recorder.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-assessment-recorder.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-assessment-recorder.min.css`;
@@ -11680,7 +11680,7 @@ ${JSON.stringify(parsedContext)}` : "");
     return `${CDN_BASE_PATH}@master/dist/production/styles/strigo-assessment-recorder.min.css`;
   }
   function generateAssessmentRecorderURL() {
-    return window.Strigo.isDevelopment() ? "http://local.strigo.io:7015" : ASSESSMENT_RECORDER_URL;
+    return window.Strigo.isDevelopment() ? RECORDER_LOCAL_URL : ASSESSMENT_RECORDER_URL;
   }
   function isRecordingUrlParamExists() {
     const { search } = window.location;
@@ -12824,7 +12824,7 @@ ${JSON.stringify(parsedContext)}` : "");
       this.config = {};
     }
     isDevelopment() {
-      return true;
+      return false;
     }
     init() {
       try {
