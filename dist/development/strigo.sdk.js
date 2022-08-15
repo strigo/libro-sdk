50b39dd35f4e2e02c928d0f3746ef2aa700416a0
hidday <hiddaysaban@gmail.com>
2022-08-15 15:28:47 +0300

    [semver:patch] Fix the way we calculate the overlay element positioning to support scrolling. Add Aborting action to recorder.

--- dist/development/strigo.sdk.js
+++ dist/development/strigo.sdk.js
@@ -11924,7 +11924,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7002"}/styles/strigo.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo.min.css`;
@@ -11933,7 +11933,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateWidgetCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7002"}/styles/strigo-widget.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-widget.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-widget.min.css`;
@@ -11942,7 +11942,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateAcademyHatCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7002"}/styles/strigo-academy-hat.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-academy-hat.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-academy-hat.min.css`;
@@ -11951,7 +11951,7 @@ ${JSON.stringify(parsedContext)}` : "");
   }
   function generateRecorderCssURL(version) {
     if (window.Strigo.isDevelopment()) {
-      return `${"http://local.strigo.io:7002"}/styles/strigo-assessment-recorder.css`;
+      return `${SDK_LOCAL_URL}/styles/strigo-assessment-recorder.css`;
     }
     if (version) {
       return `${CDN_BASE_PATH}@${version}/dist/production/styles/strigo-assessment-recorder.min.css`;
@@ -11959,7 +11959,7 @@ ${JSON.stringify(parsedContext)}` : "");
     return `${CDN_BASE_PATH}@master/dist/production/styles/strigo-assessment-recorder.min.css`;
   }
   function generateAssessmentRecorderURL() {
-    return window.Strigo.isDevelopment() ? "http://local.strigo.io:7015" : ASSESSMENT_RECORDER_URL;
+    return window.Strigo.isDevelopment() ? RECORDER_LOCAL_URL : ASSESSMENT_RECORDER_URL;
   }
   function isRecordingUrlParamExists() {
     const { search } = window.location;
@@ -13208,7 +13208,7 @@ ${JSON.stringify(parsedContext)}` : "");
       this.config = {};
     }
     isDevelopment() {
-      return true;
+      return false;
     }
     init() {
       try {
